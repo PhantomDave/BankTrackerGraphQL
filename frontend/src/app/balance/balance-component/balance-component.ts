6a8734b0ef967562aead0fa3e21488ed3273507f
PhantomDave <davide.rodo@pdave.dev>
2025-11-11 15:30:30 +0000

    feat: Enhance balance component with total recurring expenses and average daily calculations

--- frontend/src/app/balance/balance-component/balance-component.ts
+++ frontend/src/app/balance/balance-component/balance-component.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
 import { FinanceRecordService } from '../../models/finance-record/finance-record-service';
+import { FinanceRecord } from '../../models/finance-record/finance-record';
 
 @Component({
   selector: 'app-balance-component',
@@ -10,42 +11,70 @@ import { FinanceRecordService } from '../../models/finance-record/finance-record
 })
 export class BalanceComponent implements OnInit {
   private readonly financeRecordService = inject(FinanceRecordService);
-
+  private readonly records = signal<readonly FinanceRecord[]>(
+    this.financeRecordService.financeRecords(),
+  );
   readonly balance = computed(() => {
-    const records = this.financeRecordService.financeRecords();
+    const records = this.records();
     const total = records.reduce((sum, record) => sum + record.amount, 0);
     return total;
+  });
+
+  today = new Date();
+  daysPassed = this.today.getDate();
+
+  readonly averageDailyExpense = computed(() => {
+    return (
+      this.records()
+        .filter((record) => {
+          const recordDate = new Date(record.date);
+          return (
+            recordDate.getMonth() === this.today.getMonth() &&
+            recordDate.getFullYear() === this.today.getFullYear()
+          );
+        })
+        .reduce((sum, record) => {
+          if (record.amount < 0) return sum + record.amount;
+          return sum;
+        }, 0) / this.daysPassed
+    );
+  });
+
+  readonly averageDailyIncome = computed(() => {
+    return (
+      this.records()
+        .filter((record) => {
+          const recordDate = new Date(record.date);
+          return (
+            recordDate.getMonth() === this.today.getMonth() &&
+            recordDate.getFullYear() === this.today.getFullYear()
+          );
+        })
+        .reduce((sum, record) => {
+          if (record.amount > 0) return sum + record.amount;
+          return sum;
+        }, 0) / 31
+    );
+  });
+
+  readonly totalRecurringExpenses = computed(() => {
+    return this.records()
+      .filter((record) => record.recurring && record.amount < 0)
+      .reduce((sum, record) => sum + record.amount, 0);
   });
 
   readonly lastUpdated = signal<Date>(new Date());
 
   constructor() {
     effect(() => {
-      const records = this.financeRecordService.financeRecords();
       this.lastUpdated.set(new Date());
     });
   }
 
   readonly endOfMonthPrediction = computed(() => {
-    const records = this.financeRecordService.financeRecords();
-    const today = new Date();
-    const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
-    const daysPassed = today.getDate();
-    const averageExpensePerDay =
-      records
-        .filter((record) => {
-          const recordDate = new Date(record.date);
-          return (
-            recordDate.getMonth() === today.getMonth() &&
-            recordDate.getFullYear() === today.getFullYear()
-          );
-        })
-        .reduce((sum, record) => {
-          if (record.amount < 0) return sum + record.amount;
-          return sum;
-        }, 0) / daysPassed;
-    const remainingDays = daysInMonth - daysPassed;
-    return averageExpensePerDay * remainingDays + this.balance();
+    const daysInMonth = new Date(this.today.getFullYear(), this.today.getMonth() + 1, 0).getDate();
+    const remainingDays = daysInMonth - this.daysPassed;
+    return this.averageDailyExpense() * remainingDays + this.balance();
   });
 
   async ngOnInit(): Promise<void> {
